34ed4a8d351cb6e65fc1d4c0b56ca9c24ce98c7c
Rajeev Ravindran <razorrajeev@gmail.com>
2016-05-03 02:39:51 +0530

    Generate random MAC addresses
    Created a custom Hex printing function to generate valid MAC addresses

--- script.sh
+++ script.sh
@@ -1,7 +1,24 @@
-_deviceID=$(cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2)
-_macEth=$(ifconfig eth0 | awk '/HWaddr/ {print $5}')
-_macWlan=$(ifconfig wlan0 | awk '/HWaddr/ {print $5}')
+
+n=1
+
+echo 'Enter number of devices'
+read n
+
+echo Generating $n device info
+
+function hexPrinter(){
+randomInteger=$[ ($RANDOM % 100 ) + 16]
+randomHex=$(printf '%x\n' $randomInteger)
+echo $randomHex;
+}
+
+for ((i=1;i<=$n;i++))
+do
+_deviceID=$i;
+_macEth=$(hexPrinter):$(hexPrinter):$(hexPrinter):$(hexPrinter):$(hexPrinter):$(hexPrinter)
+_macWlan=$(hexPrinter):$(hexPrinter):$(hexPrinter):$(hexPrinter):$(hexPrinter):$(hexPrinter)
 #echo $_deviceID
 #echo $_macEth
 #echo $_macWlan
 echo  \{\"id\":\"$_deviceID\"\,\"etho\":\"$_macEth\"\,\"wlan0\":\"$_macWlan\"\}
+done
